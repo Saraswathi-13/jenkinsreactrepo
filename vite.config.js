d55a28c8d924c56e544ba133fef6551459d517b5
Saraswathi-13 <2300032530@kluniversity.in>
2025-08-04 10:36:40 +0530

    modified Dashboard

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'jenkinserp', //deployment name not repo name
+  base: 'jenkinserp', //deployment folder name not repo name
 })
